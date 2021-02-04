6845de370f37367489268a09f34d4ca4f75497d3
Sergio Delgado <sergio.e.delgado99@gmail.com>
2021-02-03 23:13:39 -0500

    feature: section proyects style

--- src/components/organisms/section-about/index.js
+++ src/components/organisms/section-about/index.js
@@ -1,25 +1,25 @@
 const SectionAbout = () => {
   return (
     <section className="o-about">
-      <h1 className="o-about__title">About</h1>
-      <p className="o-about__description">
-        Far far away, behind the word mountains, far from the countries Vokalia
-        and Consonantia, there live the blind texts.
-      </p>
+      <h2 className="o-about__title">Work experience</h2>
+      <div className="o-about-works">
+        {[1, 2, 3, 4].map(() => (
+          <div className="o-about-work">
+            <h3 className="o-about-work__position">Senior Web Designer</h3>
+            <p className="o-about-work__agency">XYZ DESIGNER</p>
+            <p className="o-about-work__date">2012-2014</p>
+          </div>
+        ))}
+      </div>
       <div className="o-about-wrapper">
         <div className="o-about-img">
           <img
-            src="https://via.placeholder.com/480x650"
+            src="https://picsum.photos/id/1//480/650"
             className="o-about-img__img"
           />
-          <div className="o-about-img__overlay">
-            <p className="overlay__title">12 Years of experience</p>
-            <a className="overlay__anchor">→ See Portafolio </a>
-            <a className="overlay__anchor">→ Download CV</a>
-          </div>
         </div>
         <div className="o-about-bio">
-          <h2 className="o-about-bio__title">Bio</h2>
+          <h2 className="o-about__title">Bio</h2>
           <p className="o-about-bio__description ">
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia, there live the blind texts. Separated they
@@ -33,16 +33,6 @@ const SectionAbout = () => {
             regelialia. It is a paradisematic country, in which roasted parts of
             sentences fly into your mouth.
           </p>
-          <h2 className="o-about-bio__title">Work experience</h2>
-          <div className="o-about-works">
-            {[1, 2, 3, 4].map(() => (
-              <div className="o-about-work">
-                <h3 className="o-about-work__position">Senior Web Designer</h3>
-                <p className="o-about-work__agency">XYZ DESIGNER</p>
-                <p className="o-about-work__date">2012-2014</p>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
     </section>
--- src/components/organisms/section-proyects/index.js
+++ src/components/organisms/section-proyects/index.js
@@ -1,19 +1,47 @@
 const proyects = [
-  { img: "https://via.placeholder.com/300x300.png", title: " title" },
-  { img: "https://via.placeholder.com/300x400.png", title: " title" },
-  { img: "https://via.placeholder.com/300x300.png", title: " title" },
-  { img: "https://via.placeholder.com/300x400.png", title: " title" },
-  { img: "https://via.placeholder.com/300x300.png", title: " title" },
-  { img: "https://via.placeholder.com/300x400.png", title: " title" },
+  {
+    img: "https://picsum.photos/300/400/",
+    title: " portfolio",
+    description: "react.js, next.js",
+  },
+  {
+    img: "https://picsum.photos/300/400/",
+    title: " portfolio",
+    description: "react.js, next.js",
+  },
+  {
+    img: "https://picsum.photos/300/400/",
+    title: " portfolio",
+    description: "react.js, next.js",
+  },
+  {
+    img: "https://picsum.photos/300/400/",
+    title: " portfolio",
+    description: "react.js, next.js",
+  },
+  {
+    img: "https://picsum.photos/300/400/",
+    title: " portfolio",
+    description: "react.js, next.js",
+  },
+  {
+    img: "https://picsum.photos/300/400/",
+    title: " portfolio",
+    description: "react.js, next.js",
+  },
 ];
 
 const SectionProyects = () => {
   return (
     <section className="o-proyects">
-      {proyects.map((proyect) => {
+      {proyects.map(({ img, title, description }) => {
         return (
           <article className="o-proyects__item">
-            <img src={proyect.img} />
+            <img src={img} className="o-proyects__img" />
+            <div className="o-proyects-body">
+              <h1 className="o-proyects__title"> {title} </h1>
+              <p className="o-proyects__description">{description}</p>
+            </div>
           </article>
         );
       })}
